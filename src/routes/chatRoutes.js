2cfa58129e4442fcfc25e1a9b99c0e6df1842653
priyankamakwana22 <priyankatech68@gmail.com>
2025-05-02 15:33:56 +0530

    final changes

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const { sendMessage } = require("../controller/message.controller");
-const authenticateToken = require("../middleware/authMiddleware");
-const validateRequest = require("../middleware/validateRequest");
-const authSchema = require("../validator/auth.validator");
-router.post("/", authenticateToken, validateRequest(authSchema), sendMessage);
+const express = require('express')
+const router = express.Router()
+const { sendMessage } = require('../controller/message.controller')
+const authenticateToken = require('../middleware/authMiddleware')
+const validateRequest = require('../middleware/validateRequest')
+const authSchema = require('../validator/auth.validator')
+router.post('/', authenticateToken, sendMessage)
 
-module.exports = router;
+module.exports = router
--- src/validator/auth.validator.js
+++ src/validator/auth.validator.js
@@ -1,9 +1,9 @@
-const Joi = require("joi");
+const Joi = require('joi')
 
 const authSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
+  password: Joi.string().min(8).required(),
+})
 
-module.exports = authSchema;
+module.exports = authSchema
